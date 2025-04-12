8cdfa2f82881fa0c405cfef5f36dd4676b9ece28
Quentin <der.hookipianer@web.de>
2025-04-12 15:32:04 +0200

    improved question prompts

--- src/lib/components/Prompt.svelte
+++ src/lib/components/Prompt.svelte
@@ -4,20 +4,16 @@
     const {
         prompt,
         changeAnswer,
-        skipQuestion,
     }: {
         prompt: QuestionPrompt;
-        changeAnswer: (index: number) => void;
-        skipQuestion: () => void;
+        changeAnswer: (index?: number) => void;
     } = $props();
 
-
-    const colors = ['bg-amber-200', 'bg-rose-200', 'bg-lime-200', 'bg-sky-200'];
+    const colors = ["bg-amber-200", "bg-rose-200", "bg-lime-200", "bg-sky-200"];
 
     function randomColor() {
-      return colors[Math.floor((Math.random() * colors.length))];
+        return colors[Math.floor(Math.random() * colors.length)];
     }
-
 </script>
 
 <div
@@ -27,11 +23,12 @@
         {prompt.question.text}
     </p>
     {#if prompt.chosenAnswer !== undefined}
-        <div
-            class="mt-4 p-4 border-2 border-black rounded-md shadow-[2px_2px_0px_0px_rgba(0,0,0)] bg-white"
+        <button
+            onclick={() => changeAnswer(undefined)}
+            class=" cursor-pointer mt-4 p-4 border-2 w-full border-black rounded-md shadow-[2px_2px_0px_0px_rgba(0,0,0)] bg-white"
         >
             {prompt.question.answerTags[prompt.chosenAnswer]}
-        </div>
+        </button>
     {:else}
         <div class="mt-4 grid grid-cols-2 md:grid-cols-3 gap-4">
             {#each prompt.question.answerTags as answer, index}
@@ -45,12 +42,5 @@
                 </button>
             {/each}
         </div>
-
-        <button
-            class="mt-6 bg-gray-300 border-2 border-black rounded-md shadow-[4px_4px_0px_0px_rgba(0,0,0)] px-4 py-2 cursor-pointer hover:bg-gray-400 active:shadow-[1px_1px_0px_0px_rgba(0,0,0)] active:translate-x-[3px] active:translate-y-[3px]"
-            onclick={skipQuestion}
-        >
-            Skip
-        </button>
     {/if}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
     import Prompt from "$lib/components/Prompt.svelte";
     import { hobbyFinderQuestions } from "$lib/questions";
     import type { QuestionPrompt } from "$lib/types";
-
     const questions: QuestionPrompt[] = $state([]);
 
     hobbyFinderQuestions.forEach((q) => {
@@ -13,7 +12,8 @@
         let paramText = "";
         questions.forEach((q) => {
             if (q.chosenAnswer !== undefined) {
-                paramText = paramText + q.question.answerTags[q.chosenAnswer] + '&';
+                paramText =
+                    paramText + q.question.answerTags[q.chosenAnswer] + "&";
             }
         });
         return paramText.slice(0, -1);
@@ -23,23 +23,43 @@
 </script>
 
 <ul class="">
-    {#each questions as pair, promptIndex}
-        <li class="py-4">
-            <Prompt
-                prompt={pair}
-                changeAnswer={(index) => {
-                    pair.chosenAnswer = index;
-                    if (promptIndex === currentPrompt) {
-                        currentPrompt++;
-                    }
-                }}
-                skipQuestion={() => {
-                    currentPrompt++;
-                }}
-            />
-        </li>
-    {/each}
-    <li class=" flex w-full justify-center items-center mb-20">
+    <div class=" flex">
+        {#each questions as question}
+            {#if questions[currentPrompt] === question}
+                <li class="py-4">
+                    <Prompt
+                        prompt={question}
+                        changeAnswer={(index) => {
+                            question.chosenAnswer = index;
+                            if (index !== undefined && currentPrompt + 1 < questions.length) {
+                                currentPrompt++;
+                            }
+                        }}
+                    />
+                </li>
+            {/if}
+        {/each}
+    </div>
+
+    <li class=" flex w-full justify-center items-center">
+        {#if currentPrompt > 0}
+            <button
+                class="mt-6 bg-gray-300 border-2 border-black rounded-md shadow-[4px_4px_0px_0px_rgba(0,0,0)] px-4 py-2 cursor-pointer hover:bg-gray-400 active:shadow-[1px_1px_0px_0px_rgba(0,0,0)] active:translate-x-[3px] active:translate-y-[3px]"
+                onclick={() => currentPrompt--}
+            >
+                Prev
+            </button>
+        {/if}
+        {#if currentPrompt + 1 < questions.length}
+            <button
+                class="mt-6 bg-gray-300 border-2 border-black rounded-md shadow-[4px_4px_0px_0px_rgba(0,0,0)] px-4 py-2 cursor-pointer hover:bg-gray-400 active:shadow-[1px_1px_0px_0px_rgba(0,0,0)] active:translate-x-[3px] active:translate-y-[3px]"
+                onclick={() => currentPrompt++}
+            >
+                Skip
+            </button>
+        {/if}
+    </li>
+    <li class="flex w-full justify-center items-center mb-20">
         <a
             href={"results?" + resultParams}
             class="mt-6 bg-red-500 border-2 font-black text-xl border-black rounded-md shadow-[4px_4px_0px_0px_rgba(0,0,0)] px-4 py-2 cursor-pointer hover:bg-gray-400 active:shadow-[1px_1px_0px_0px_rgba(0,0,0)] active:translate-x-[3px] active:translate-y-[3px]"
