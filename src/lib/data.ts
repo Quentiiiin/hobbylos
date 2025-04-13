ae70d44fc6bdb766ad90a4aa39d257f0bab7e5a1
Quentin <der.hookipianer@web.de>
2025-04-13 10:13:13 +0200

    improved matchmaking

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -7,22 +7,22 @@ export const hobbies: Hobby[] = [];
 
 export const questions: Question[] = [];
 
-for(const h in hobbyFiles) {
+for (const h in hobbyFiles) {
     const hobbyMaybe = hobbyFiles[h];
     const abc = JSON.parse(JSON.stringify(hobbyMaybe)); // fucking weird bs required to make zod work
     const parseResult = HobbySchema.safeParse(abc);
-    if(parseResult.success) {
+    if (parseResult.success) {
         hobbies.push(parseResult.data);
     } else {
         console.error('Error reading hobby file:', h, parseResult.error);
     }
 }
 
-for(const q in questionFiles) {
+for (const q in questionFiles) {
     const questionMaybe = questionFiles[q];
     const abc = JSON.parse(JSON.stringify(questionMaybe)); // fucking weird bs required to make zod work
     const parseResult = QuestionSchema.safeParse(abc);
-    if(parseResult.success) {
+    if (parseResult.success) {
         questions.push(parseResult.data);
     } else {
         console.error('Error reading question file:', q, parseResult.error);
@@ -41,24 +41,8 @@ export function getAllTags() {
     const tags: string[] = [];
     hobbies.forEach((h) => {
         h.tags.forEach((t) => {
-            if(!tags.includes(t.tag)) tags.push(t.tag);
+            if (!tags.includes(t.tag)) tags.push(t.tag);
         });
     });
     return tags;
-}
-
-export function match(inputTags: string[]): Result[] {
-    const results: Result[] = [];
-
-    for (const hobby of hobbies) {
-        const overlap = hobby.tags.filter(tag => inputTags.includes(tag.tag)).length;
-        const totalTags = hobby.tags.length;
-        const percent = totalTags === 0 ? 0 : (overlap / totalTags) * 100;
-
-        results.push({
-            hobby: hobby,
-            matchRate: Math.round(percent)
-        });
-    }
-    return results;
 }
--- src/routes/results/+page.ts
+++ src/routes/results/+page.ts
@@ -1,13 +1,18 @@
-import { hobbies, match } from '$lib/data';
+import { match } from '$lib/matchmaking';
 import type { Result } from '$lib/types';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = ({ params, url }) => {
 
     const tags: string[] = Array.from(url.searchParams.keys());
+    const tagsWithValue: Record<string, number> = {};
+
+    tags.forEach(t => {
+        tagsWithValue[t] = Number.parseInt(url.searchParams.get(t) ?? '1') ?? 1;
+    });
 
 
-    const res: Result[] = match(tags);
+    const res: Result[] = match(tagsWithValue);
 
     const sortedRes = res.toSorted((a, b) => b.matchRate - a.matchRate); //sort by matchrate
 
