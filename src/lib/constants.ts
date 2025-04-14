d910654a1d74c3bacdc684081538acabedae3920
Quentin <der.hookipianer@web.de>
2025-04-14 18:55:25 +0200

    added structutured data for hobby page

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,7 @@
 export const SITE_FRIENDLY_NAME = 'hobbylos';
 
+export const BASE_URL = 'https://hobbylos.quentins.space';
+
 export const ALREADY_KNOWN_TAG_PENALTY = 5;
 export const POTENTIAL_HOBBY_TAG_REWARD = 1;
 
--- src/routes/hobby/[slug]/+page.svelte
+++ src/routes/hobby/[slug]/+page.svelte
@@ -1,13 +1,33 @@
 <script lang="ts">
     import HobbyCard from "$lib/components/HobbyCard.svelte";
     import RelatedHobbiesBox from "$lib/components/RelatedHobbiesBox.svelte";
-    import { SITE_FRIENDLY_NAME } from "$lib/constants.js";
+    import { BASE_URL, SITE_FRIENDLY_NAME } from "$lib/constants.js";
 
     const { data } = $props();
 
     let keywords = $state("");
     data.hobby?.tags.forEach((t) => {
         keywords = keywords + ", " + t.tag;
+    });
+
+    const schema: any = {
+        "@context": "https://schema.org",
+        "@type": "CreativeWork",
+        name: data.hobby?.name,
+        slug: data.hobby?.slug,
+        // svelte-ignore state_referenced_locally
+        keywords: keywords,
+        description: data.hobby?.description,
+        url: `${BASE_URL}/hobby/${data.hobby?.slug}`,
+        hasPart: [],
+    };
+
+    data.hobby?.resources.forEach((r) => {
+        schema.hasPart.push({
+            "@type": "WebPage",
+            name: r.name,
+            url: r.url,
+        });
     });
 </script>
 
@@ -19,13 +39,14 @@
     <meta name="description" content={data.hobby?.description} />
     <meta property="og:title" content={data.hobby?.name} />
     <meta property="og:description" content={data.hobby?.description} />
+    {@html `<script type="application/ld+json">${JSON.stringify(schema)}</script>`}
 </svelte:head>
 
 <div class=" flex flex-col">
     {#if data.hobby}
-    <HobbyCard hobby={data.hobby} />
-    {#if data.relatedHobbies}
-    <RelatedHobbiesBox hobbies={data.relatedHobbies} />
+        <HobbyCard hobby={data.hobby} />
+        {#if data.relatedHobbies}
+            <RelatedHobbiesBox hobbies={data.relatedHobbies} />
+        {/if}
     {/if}
-{/if}
 </div>
